d0b7a1d8170427499ef1959ceff25317faa709bf
Pedro Garrido <pedro.munoz.garrido@cftsa.cl>
2022-02-05 23:20:48 -0300

    node mailer bugged

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,30 @@
+require('dotenv').config();
+const PASSWORD = process.env.password;
+
 export default function handler(req, res) {
-  console.log(req.body);
+  let nodemailer = require('nodemailer');
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user:'velcaik',
+      pass: PASSWORD,
+    },
+    secure: true,
+  });
+  const mailData={
+    from: 'velcaik@gmail.com',
+    to: 'belcaik@gmail.com',
+    subject: `Mensaje de ${req.body.nombre}`,
+    text: req.body.mensaje,
+    html: <div>{req.body.message}</div>
+  }
+  transporter.sendMail(mailData, (err, info)=>{
+    if(err){
+      console.log(err)
+    }else{
+      console.log(info)
+    }
+  })
+  res.status(200)
 }
