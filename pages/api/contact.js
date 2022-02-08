20f5bfa445a7f41cb9a1ba78dc2f6fc4a301e98a
Pedro Garrido <pedro.munoz.garrido@cftsa.cl>
2022-02-08 20:37:23 -0300

    fix mail sender

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,31 +1,49 @@
 require('dotenv').config();
 const PASSWORD = process.env.password;
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 export default function handler(req, res) {
   var transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: "smtp.pedrogarrido.tech",
     port: 587,
-    secure: false,
+    debug: true,
+    logger: true,
+    tls: {
+      secureProtocol: "TLSv1_method",
+      secure: false,
+      ignoreTLS: true,
+      rejectUnauthorized: false,
+    },
     auth: {
-      user:'louisa.wolf34@ethereal.email',
+      user: "contacto@pedrogarrido.tech",
       pass: PASSWORD,
+    },
+  });
+  var mailData = {
+    from: "contacto@pedrogarrido.tech",
+    to: "belcaik@gmail.com",
+    subject: `${req.body.nombre} Te envio un mensaje!`,
+    html: `
+      <h2>Mensaje:</h2>
+      <p>${req.body.mensaje}</p>
+      <h3>datos de contacto</h3>
+      <ul>
+        <li>Nombre: ${req.body.nombre}</li>
+        <li>Email: ${req.body.email}</li>
+        <li></li>
+      </ul>
+      <footer>
+        este correo se envia automaticamente porfavor no responder
+      <footer>
+    `,
+    // html: <div>{req.body.message}</div>
+  };
+  transporter.sendMail(mailData, (err, info) => {
+    if (err) {
+      res.status(500);
+      console.log(err);
+    } else {
+      res.status(200).then(console.log(info));
     }
   });
-  var mailData={
-    from: 'louisa.wolf34@ethereal.email',
-    to: 'belcaik@gmail.com',
-    subject: `Mensaje de `,
-    text: 'sdfasd',
-   // html: <div>{req.body.message}</div>
-  }
-  transporter.sendMail(mailData, (err, info)=>{
-    if(err){
-      console.log(err)
-      res.status(500)
-    }else{
-      console.log(info)
-      res.status(200)
-    }
-  })
 }
